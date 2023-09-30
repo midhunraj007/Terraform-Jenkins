f723430ff3a0fdacb2b4c8f0a1869b7978aea176
midhunraj007 <59755649+midhunraj007@users.noreply.github.com>
2023-09-30 10:26:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  
+  region  = "ca-central-1"
+  access_key = AKIAT532D4I4P7VRD4PI""
+  secret_key = "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"
+  profile = "default"
 }
 
 resource "aws_instance" "foo" {
